e23494ab15d6876a4f94a4a626a6b8aacc568fd0
Taylor Hadean <taylorhadean@gmail.com>
2017-06-20 15:29:27 -0600

    Functioning Calculator

--- rubycalc2.rb
+++ rubycalc2.rb
@@ -1,36 +1,27 @@
+def calc
+  puts "Ruby Calc 2.0 \nEnter e to exit Calc"
 
-def divide
-  puts ">> "
-end
+  while true
+  print "> "
+  str = gets.chomp.split(" ")
+  return if str[0]== 'e'
 
+  num1 = str[0].to_f
+  num2 = str[2].to_f
+  numop = str[1].to_sym
 
-
-
-def menu
-  choice = gets.strip.to_i
-  case choice
-    when 1
-      add
-    when 2
-      subtract
-    when 3
-      multiply
-    when 4
-      divide
-
+  case numop
+    when :+
+      then puts num1 + num2
+    when :-
+      then puts num1 - num2
+    when :*
+      then puts num1 * num2
+    when :/
+      then puts num1 / num2
+    else
+      puts "Invalid Input"
+    end
   end
 end
-
-
-
-
-def print_menu
-  puts "My Calc \nWould you like to:"
-  puts "1)Add"
-  puts "2)Subtract"
-  puts "3)Multiply"
-  puts "4)Divide"
-  menu
-end
-
-print_menu
+calc
